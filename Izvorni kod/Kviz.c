ed4dc4ba9f5b56f54af9fa3e8eacca88c444e0d5
SimeonTM <58887448+SimeonTM@users.noreply.github.com>
2020-01-13 12:01:59 +0100

    Uredjen kviz

--- Izvorni kod/Kviz.c
+++ Izvorni kod/Kviz.c
@@ -1,123 +1,126 @@
 #include "Kviz.h"
 
 
-   void Read_Quiz(KVIZ *kviz)
-   {
-       int i;
-       char pom[101]={};
-       FILE *fp=fopen("Kviz.txt","r");
 
-       if(fp == NULL)  return;
+void Read_Quiz(KVIZ *kviz)
+{
+    int i;
+    char pom[101]={};
+    FILE *fp=fopen("Kviz.txt","r");
 
-       fscanf(fp,"%d\n",&kviz->length);  //ucitaj broj pitanja
+    if(fp == NULL)  return;
 
-       for(i=0;i<kviz->length;i++)        //citanje kviza oz fajla
-       {
-           fscanf(fp,"%s\n",pom);
-           strcpy(kviz->question[i],pom);
+    fscanf(fp,"%d\n",&kviz->length);  //ucitaj broj pitanja
 
-           fscanf(fp,"%s\n",pom);
-           strcpy(kviz->first_answer[i],pom);
-
-           fscanf(fp,"%s\n",pom);
-           strcpy(kviz->second_answer[i],pom);
-
-           fscanf(fp,"%s\n",pom);
-           strcpy(kviz->third_answer[i],pom);
-
-           fscanf(fp,"%d\n",&kviz->correct[i]);
-       }
-       fclose(fp);
-   }
-
-
-
-    void Play_Quiz(KVIZ *kviz)
+    for(i=0;i<kviz->length;i++)        //citanje kviza oz fajla
     {
-         printf("\n******DOBRO DOSLI U KVIZ-zabavite se i proverite svoje znanje!!!******\n");
-         int i;
-         int input,counter=0;
+        fscanf(fp,"%s\n",pom);      strcpy(kviz->question[i],pom);
+        fscanf(fp,"%s\n",pom);      strcpy(kviz->first_answer[i],pom);
+        fscanf(fp,"%s\n",pom);      strcpy(kviz->second_answer[i],pom);
+        fscanf(fp,"%s\n",pom);      strcpy(kviz->third_answer[i],pom);
+        fscanf(fp,"%d\n",&kviz->correct[i]);
+    }
+    fclose(fp);
+}
 
-         for(i=0;i<kviz->length;i++)
+
+void Remove_Underscores(char *string)
+{
+    for (int i=0; string[i]; i++)
+        if(string[i]=='_')
+            string[i]=' ';
+}
+
+void Play_Quiz(KVIZ *kviz)
+{
+     printf("\n******DOBRO DOSLI U KVIZ-zabavite se i proverite svoje znanje!!!******\n");
+     int i;
+     int input,counter=0;
+
+     for(i=0;i<kviz->length;i++)
+     {
+         Remove_Underscores(kviz->question[i]       );     printf("\n %d. pitanje: %s\n",i+1,kviz->question[i]);
+         Remove_Underscores(kviz->first_answer[i]   );     printf("    - 1. odgovor: %s\n",kviz->first_answer[i]);
+         Remove_Underscores(kviz->second_answer[i]  );     printf("    - 2. odgovor: %s\n",kviz->second_answer[i]);
+         Remove_Underscores(kviz->third_answer[i]   );     printf("    - 3. odgovor: %s\n",kviz->third_answer[i]);
+
+         do
          {
-             printf("\n%d.pitanje:%s\n",i+1,kviz->question[i]);
-             printf("  1.odgovor:%s\n",kviz->first_answer[i]);
-             printf("  2.odgovor:%s\n",kviz->second_answer[i]);
-             printf("  3.odgovor:%s\n",kviz->third_answer[i]);
+             printf(" Vas odgovor je: ");
+             scanf("%d",&input);
+         }while(input != 1 && input != 2 && input != 3);
 
-
-             do
-             {
-                 printf("Vas odgovor je:");
-                 scanf("%d",&input);
-             }while(input != 1 && input != 2 && input != 3);
-
-             if(input == kviz->correct[i])
-             {
-                 printf("Pogodili ste!\n");
-                 ++counter;
-             }
-             else printf("Tacan odgovor je pod %d.\n",kviz->correct[i]);
+         if(input == kviz->correct[i])
+         {
+             printf(" Pogodili ste!\n");
+             ++counter;
          }
+         else printf(" Tacan odgovor je pod %d.\n",kviz->correct[i]);
+     }
 
-         printf("\nImali ste %d/%d tacnih odgovora!!!\n",counter,kviz->length);
-         printf("Hvala vam sto ste ucestvovali.\n");
-         printf("**********************************************************************\n");
+     printf("\nImali ste %d/%d tacnih odgovora!!!\n",counter,kviz->length);
+     printf("Hvala vam sto ste ucestvovali.\n");
+     printf("**********************************************************************\n");
+
+}
+
+
+
+void Delete_Question(KVIZ *kviz,int index)
+{
+    index-=1;//indeks  pitanja koje se brise u kvizu;
+    int last=kviz->length-1;
+    if(index != kviz->length-1)  //na mjesto pitanja koje se brise se kopira
+    {                            //zadnje pitanje
+
+        strcpy(kviz->question[index],kviz->question[last]);
+        strcpy(kviz->first_answer[index],kviz->first_answer[last]);
+        strcpy(kviz->second_answer[index],kviz->second_answer[last]);
+        strcpy(kviz->third_answer[index],kviz->third_answer[last]);
+        kviz->correct[index]=kviz->correct[last];
 
     }
 
+    --kviz->length;     //smanjuje se broj pitanja
+}
 
 
-    void Delete_Question(KVIZ *kviz,int index)
+void Add_Question( KVIZ *kviz,
+                   const char *question,
+                   const char *first_ans,
+                   const char *second_ans,
+                   const char *third_ans,
+                   int correct )
+{
+    int index = kviz->length;  //funkcija dodaje novo pitanje u kviz
+
+    strcpy(kviz->question[index],question);
+    strcpy(kviz->first_answer[index],first_ans);
+    strcpy(kviz->second_answer[index],second_ans);
+    strcpy(kviz->third_answer[index],third_ans);
+    kviz->correct[index] = correct;
+
+    ++kviz->length;   //povecava se ukupan broj pitanja
+}
+
+
+void Write_Quiz(KVIZ *kviz)    //sadrzaj kviza se upisuje u fajl;
+{
+
+    int i;
+    FILE *fp=fopen("Kviz.txt","w");
+    if(fp == NULL)  return;
+
+    fprintf(fp,"%d\n",kviz->length);  //upis broja pitanja
+
+    for(i=0;i<kviz->length;i++)
     {
-        index-=1;//indeks  pitanja koje se brise u kvizu;
-        int last=kviz->length-1;
-        if(index != kviz->length-1)  //na mjesto pitanja koje se brise se kopira
-        {                            //zadnje pitanje
-
-            strcpy(kviz->question[index],kviz->question[last]);
-            strcpy(kviz->first_answer[index],kviz->first_answer[last]);
-            strcpy(kviz->second_answer[index],kviz->second_answer[last]);
-            strcpy(kviz->third_answer[index],kviz->third_answer[last]);
-            kviz->correct[index]=kviz->correct[last];
-
-        }
-
-        --kviz->length;     //smanjuje se broj pitanja
+        fprintf(fp,"%s\n",kviz->question[i]);
+        fprintf(fp,"%s\n",kviz->first_answer[i]);
+        fprintf(fp,"%s\n",kviz->second_answer[i]);
+        fprintf(fp,"%s\n",kviz->third_answer[i]);
+        fprintf(fp,"%d\n",kviz->correct[i]);
     }
-
-
-    void Add_Question(KVIZ *kviz,const char *question,const char *first_ans,const char *second_ans,const char *third_ans,int correct)
-    {
-        int index = kviz->length;  //funkcija dodaje novo pitanje u kviz
-
-        strcpy(kviz->question[index],question);
-        strcpy(kviz->first_answer[index],first_ans);
-        strcpy(kviz->second_answer[index],second_ans);
-        strcpy(kviz->third_answer[index],third_ans);
-        kviz->correct[index] = correct;
-
-        ++kviz->length;   //povecava se ukupan broj pitanja
-    }
-
-
-    void Write_Quiz(KVIZ *kviz)    //sadrzaj kviza se upisuje u fajl;
-    {
-
-        int i;
-        FILE *fp=fopen("Kviz.txt","w");
-        if(fp == NULL)  return;
-
-        fprintf(fp,"%d\n",kviz->length);  //upis broja pitanja
-
-        for(i=0;i<kviz->length;i++)
-        {
-            fprintf(fp,"%s\n",kviz->question[i]);
-            fprintf(fp,"%s\n",kviz->first_answer[i]);
-            fprintf(fp,"%s\n",kviz->second_answer[i]);
-            fprintf(fp,"%s\n",kviz->third_answer[i]);
-            fprintf(fp,"%d\n",kviz->correct[i]);
-        }
-        fclose(fp);
-    }
+    fclose(fp);
+}
 
